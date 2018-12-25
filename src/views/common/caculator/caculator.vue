247395c46b769082c0b6442baf028862d6ea01e3
wanghongqiang <wanghongqiang@dpandora.cn>
2018-12-25 15:21:03 +0800

    commit test

--- src/views/common/caculator/caculator.vue
+++ src/views/common/caculator/caculator.vue
@@ -23,22 +23,22 @@
           <el-form-item label="出借期限">
             <el-input v-model="caculator.duration" placeholder="出借期限"
               ><template slot="append"
-                ><span class="el-icon-caret el-icon-caret-left"></span>元<span class="el-icon-caret el-icon-caret-right"></span></template
+                ><span class="el-icon-caret el-icon-caret-left" @click="changeDuration"></span><i>{{durationText}}</i><span class="el-icon-caret el-icon-caret-right" @click="changeDuration"></span></template
             ></el-input>
           </el-form-item>
           <el-form-item label="还款方式">
             <el-select v-model="caculator.type" placeholder="还款方式">
-              <el-option label="等额本息" value="1"></el-option>
-              <el-option label="先息后本" value="2"></el-option>
+              <el-option v-for="(type, i) in types" :key="i" :label="type.label" :value="type.value"></el-option>
             </el-select>
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-form-item>
         </div>
-        <div>
+        <div class="btn-wrapper">
           <el-form-item> <el-button type="primary" @click="onSubmit">开始计算</el-button> </el-form-item>
           <el-form-item> <el-button type="primary" @click="onSubmit">重置</el-button> </el-form-item>
         </div>
       </el-form>
+      <p class="light-tip">公式：本金*(年化收益率／12)*投资期限</p>
     </div>
   </div>
 </template>
@@ -48,15 +48,66 @@ export default {
   name: 'caculator',
   data() {
     return {
+      types: [
+        {
+          label: '等额本息',
+          value: 1
+        },
+        {
+          label: '先息后本',
+          value: 2
+        }
+      ],
       caculator: {
         sum: '',
         rate: '',
         duration: '',
         type: '等额本息'
+      },
+      durationText: '月'
+
+    }
+  },
+  watch: {
+    durationText(val) {
+      if (val === '日') {
+        this.caculator.type = '先息后本'
+        this.types = [
+          {
+            label: '先息后本',
+            value: 2
+          }
+        ]
+      } else {
+        this.types = [
+          {
+          label: '等额本息',
+          value: 1
+          },
+          {
+            label: '先息后本',
+            value: 2
+          }
+        ]
       }
+    },
+    caculator: {
+      handler(val){
+        if (val.type === '等额本息') {
+          this.durationText = '月'
+        }
+      },
+      deep:true
     }
   },
   methods: {
+    changeDuration() {
+      if (this.durationText === '月') {
+        this.durationText = '日'
+      } else {
+        this.durationText = '月'
+      }
+    },
     onSubmit() {
       console.log('submit!')
     }
@@ -79,10 +130,18 @@ export default {
           display: flex;
           .el-form-item {
             flex: 1;
+            .el-form-item__label{
+              font-size: 18px;
+            }
             .el-form-item__content{
               width: 250px;
               .el-input-group__append{
                 padding: 0 8px;
+                background-color: #f8f8f8;
+                span{
+                  color: #CDCDCD;
+                  cursor: pointer;
+                }
               }
             }
             .el-select {
@@ -95,7 +154,7 @@ export default {
                 border-left: 1px solid #dcdfe6;
                 border-top-right-radius: 4px;
                 border-bottom-right-radius: 4px;
-                background-color: #CDCDCD;
+                background-color: #F8F8FB;
               }
             }
             &:nth-of-type(1) {
@@ -106,8 +165,28 @@ export default {
             }
           }
         }
+        &.btn-wrapper{
+          margin: 18px 0;
+          padding-left: 85px;
+          .el-form-item{
+            .el-form-item__content{
+              width: 250px;
+              button{
+                width: 100%;
+              }
+            }
+            &:nth-of-type(2){
+              margin-left: 60px;
+              button{
+                background-color: #099EF5;
+                border-color: #099EF5;
+              }
+            }
+          }
+        }
       }
     }
   }
+
 }
 </style>
