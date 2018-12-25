e505bb04dbbba8e1d84d926c7622d2d93718e07e
alexanderhs <774844355@qq.com>
2018-12-25 16:47:59 +0800

    注册页

--- src/views/common/register/mobile.vue
+++ src/views/common/register/mobile.vue
@@ -2,10 +2,17 @@
     <div class="container">
       <div class="form-container">
         <div class="form">
-          <h1>新用户注册</h1>
-          <div class=""></div>
-          <button></button>
-          <p>同意<span>《汇有财注册协议》</span></p>
+          <h1><span>新用户注册</span></h1>
+          <div class="from-item">
+
+          </div>
+          <el-button type="primary">下一步</el-button>
+          <p class="agreement-tip">
+            <i class="iconfont" :class="{'icon-choose': !agree, 'icon-check': agree }" @click="agree = !agree"></i>
+            同意
+            <router-link class="link" to="/register/agreement">《汇有财注册协议》</router-link>
+          </p>
+          <p class="have-account">已有汇有财账号？<router-link class="link" to="/login">立即登录</router-link></p>
         </div>
       </div>
     </div>
@@ -13,7 +20,12 @@
 
 <script>
 export default {
-  name: 'mobile'
+  name: 'mobile',
+  data() {
+    return {
+      agree: true
+    }
+  }
 }
 </script>
 
@@ -30,11 +42,62 @@ export default {
     margin: 0 auto;
     padding: 35px 0;
     .form {
+      font-size: $font-size-small-s;
       width: 380px;
       height: 430px;
       border-radius: 6px;
       margin: 0 0 0 auto;
       background: #fff;
+      padding: 21px 29px 0 31px;
+      text-align: center;
+      h1 {
+        height: 20px;
+        line-height: 20px;
+        font-size: 20px;
+        padding: 6px 0;
+        margin-bottom: 58px;
+        span {
+          font-weight: 400;
+          color: rgba(74, 74, 74, 1);
+          border-bottom: 2px solid #fb9d1f;
+        }
+      }
+      .from-item {
+        width: 320px;
+        height: 40px;
+        background: rgba(255, 255, 255, 1);
+        border-radius: 2px;
+        border: 1px solid rgba(227, 227, 227, 1);
+      }
+      button {
+        margin-top: 30px;
+        width: 320px;
+        height: 40px;
+        border-radius: 2px;
+        color: #fff;
+        font-size: 18px;
+        font-weight: 400;
+      }
+      p.agreement-tip {
+        text-align: left;
+        margin-top: 20px;
+        height: 16px;
+        i {
+          cursor: pointer;
+          .icon-choose {
+            color: #cdcdcd;
+          }
+          .icon-check {
+            color: #099ef5;
+          }
+        }
+        .link {
+          color: $color-link;
+        }
+      }
+      p.have-account {
+        margin-top: 133px;
+      }
     }
   }
 }
