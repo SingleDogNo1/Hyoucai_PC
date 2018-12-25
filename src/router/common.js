ccb564921df67b2fc90176bb8ea595e75ab50833
wangzhe <wangzhe02@dpandora.cn>
2018-12-25 19:42:08 +0800

    网站公告

--- src/router/common.js
+++ src/router/common.js
@@ -1,8 +1,11 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Layout from '@/layout/layout.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Layout from '@/layout/layout.vue'
+import Share from 'vue-social-share'
+import 'vue-social-share/dist/client.css'
 
-Vue.use(Router);
+Vue.use(Router)
+Vue.use(Share)
 
 export default new Router({
 	routes: [
@@ -21,7 +24,22 @@ export default new Router({
 					name: 'announcement',
 					component: () =>
 						import(/* webpackChunkName: "announcement" */ '@/views/common/announcement/index.vue'),
-					meta: { title: '网站公告' }
+					meta: { title: '网站公告' },
+					redirect: "/announcement/index",
+					children: [
+						{
+							path: 'index',
+							name: 'announcementIndex',
+							component: () =>
+								import(/* webpackChunkName: "announcementIndex" */ '@/views/common/announcement/announcement.vue')
+						},
+						{
+							path: ':id',
+							name: 'announcementDetail',
+							component: () =>
+								import(/* webpackChunkName: "announcementDetail" */ '@/views/common/announcement/detail.vue')
+						}
+					]
 				}
 			]
 		}
