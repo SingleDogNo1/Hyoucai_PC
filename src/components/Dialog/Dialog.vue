e14638a0c8ba0db7dcf028e61bcd308ace25aa95
zhaochenmin <zhaochenmin@dpandora.cn>
2019-01-03 11:13:26 +0800

    20190103 1st

---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Dialog">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dialog',
+  mixins: [],
+  components: {},
+  data() {
+    return {
+      msg: 'Dialog'
+    }
+  },
+  props: {},
+  watch: {},
+  methods: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  destroyed() {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/css/theme';
+@import '../../assets/css/mixins';
+
+.Dialog {
+  @include cube(500px, 200px);
+  background: #000;
+  margin-top: 50%;
+  margin-left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+}
+</style>
