e0913cd8fd7677f20ab6423c5d67739b4fa00fe4
wangzhe <wangzhe02@dpandora.cn>
2019-01-04 17:42:28 +0800

    联调中

--- src/views/djs/infoDisclosure/index.vue
+++ src/views/djs/infoDisclosure/index.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="info-disclosure">
+  <div class="bg-header"><h3>信息披露中心</h3></div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +11,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .info-disclosure {
+    .bg-header {
+      width: 100%;
+      height: 200px;
+      background: url("./images/bg_header.png") center center no-repeat;
+      h3 {
+        width: 184px;
+      }
+    }
+  }
 </style>
 
