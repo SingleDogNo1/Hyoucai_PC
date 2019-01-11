301b1c470bea7061207cacf067bf6c950c25bbf8
hulixia@dpandora.cn <Jingyong666>
2019-01-11 16:58:05 +0800

    Merge branch 'dev' of ssh://gitlab.dpandora.cn:10022/Front_End_Home/Hyoucai_PC into dev_hulixia
    # Conflicts:
    #	src/views/common/SafetyMeasures/index.vue

--- src/api/hyc/Mine/coupon.js
+++ src/api/hyc/Mine/coupon.js
@@ -3,7 +3,34 @@ import qs from 'qs'
 // 我的卡券
 export function geCoupon(data) {
   return request({
-    url: '/TouchStoneServiceNew/redPacketAndCoupon/couponPacketList',
+    url: '/redPacketAndCoupon/couponPacketList',
+    method: 'post',
+    headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data)
+  })
+}
+// 历史卡券
+export function couponPacketHistory(data) {
+  return request({
+    url: '/redPacketAndCoupon/couponPacketHistory',
+    method: 'post',
+    headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data)
+  })
+}
+// 领取加息券
+export function receiveCoupon(data) {
+  return request({
+    url: '/ReceiveCoupon',
+    method: 'post',
+    headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data)
+  })
+}
+// 领取红包
+export function receiveRedPacket(data) {
+  return request({
+    url: '/ReceiveRedPacket',
     method: 'post',
     headers: { 'Content-type': 'application/x-www-form-urlencoded' },
     data: qs.stringify(data)
