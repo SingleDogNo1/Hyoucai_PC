09f0bd49d7450d30826111fc360147633f5674b0
wanghongqiang <wanghongqiang@dpandora.cn>
2019-02-11 17:24:48 +0800

    修改计算器

--- src/views/common/calculator/calculator.vue
+++ src/views/common/calculator/calculator.vue
@@ -6,34 +6,39 @@
         <el-form :inline="true" :model="calculator" class="demo-form-inline">
           <div class="input-wrapper">
             <el-form-item label="出借金额">
-              <el-input v-model="calculator.sum" placeholder="出借金额"
-                ><template slot="append"
-                  >元</template
-                ></el-input
-              >
+              <input type="text" @input="inputSum" placeholder="出借金额" ref="refSum"/>
+              <div class="unit">元</div>
             </el-form-item>
             <el-form-item label="历史平均年化收益率">
-              <el-input v-model="calculator.rate" placeholder="历史平均年化收益率"
-                ><template slot="append"
-                  >%</template
-                ></el-input
-              >
+              <input type="text" @input="inputRate" placeholder="历史平均年化收益率"  ref="refRate"/>
+              <div class="unit">%</div>
             </el-form-item>
+          </div>
+          <div class="err-wrapper">
+            <span
+              ><em v-if="errMsg.errSum">{{ errMsg.errSum }}</em></span
+            >
+            <span
+              ><em v-if="errMsg.errRate">{{ errMsg.errRate }}</em></span
+            >
           </div>
           <div class="input-wrapper">
             <el-form-item label="出借期限">
-              <el-input v-model="calculator.duration" placeholder="出借期限"
-                ><template slot="append"
-                  ><span class="el-icon-caret el-icon-caret-left" @click="changeDuration"></span><i>{{ termType | termFilter }}</i
-                  ><span class="el-icon-caret el-icon-caret-right" @click="changeDuration"></span></template
-              ></el-input>
+              <input type="text" @input="inputDuration" placeholder="出借期限"  ref="refDuration"/>
+              <div class="unit">
+                <span class="el-icon-caret el-icon-caret-left" @click="changeDuration"></span> <i>{{ termType | termFilter }}</i>
+                <span class="el-icon-caret el-icon-caret-right" @click="changeDuration"></span>
+              </div>
             </el-form-item>
             <el-form-item label="还款方式">
-              <el-select v-model="calculator.type" placeholder="还款方式">
+              <el-select v-model.trim="calculator.type" placeholder="还款方式">
                 <el-option v-for="(type, i) in types" :key="i" :label="type.label" :value="type.value"></el-option>
               </el-select>
               <el-button slot="append" icon="el-icon-search"></el-button>
             </el-form-item>
+          </div>
+          <div class="err-wrapper">
+            <span v-if="errMsg.errDuration">{{ errMsg.errDuration }}</span>
           </div>
           <div class="btn-wrapper">
             <el-form-item> <el-button type="primary" @click="submitCalc">开始计算</el-button> </el-form-item>
@@ -60,10 +65,10 @@
     </div>
     <h3 v-if="calculator.type === 1">回款明细</h3>
     <el-table v-if="calculator.type === 1" :data="tableData" class="calculator-table">
-      <el-table-column prop="term" label="期数"> </el-table-column>
-      <el-table-column prop="totalPrincipalInterest" label="回款本息(元)"> </el-table-column>
-      <el-table-column prop="principal" label="回款本金(元)"> </el-table-column>
-      <el-table-column prop="interest" label="利息(元)"> </el-table-column>
+      <el-table-column prop="term" label="期数"></el-table-column>
+      <el-table-column prop="totalPrincipalInterest" label="回款本息(元)"></el-table-column>
+      <el-table-column prop="principal" label="回款本金(元)"></el-table-column>
+      <el-table-column prop="interest" label="利息(元)"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -97,7 +102,12 @@ export default {
       expectedRevenue: 0,
       totalSum: 0,
       tableData: [],
-      system: getRetBaseURL()
+      system: getRetBaseURL(),
+      errMsg: {
+        errSum: '',
+        errRate: '',
+        errDuration: ''
+      }
     }
   },
   watch: {
@@ -149,7 +159,60 @@ export default {
         this.termType = 2
       }
     },
+    inputSum(e) {
+      e.target.value = e.target.value.replace(/[^\d.]/g, '')
+      e.target.value = e.target.value.replace(/\.{2,}/g, '.')
+      e.target.value = e.target.value
+        .replace('.', '$#$')
+        .replace(/\./g, '')
+        .replace('$#$', '.')
+      e.target.value = e.target.value.replace(/^(-)*(\d+)\.(\d\d).*$/, '$1$2.$3')
+      if (e.target.value.indexOf('.') < 0 && e.target.value !== '') {
+        e.target.value = parseFloat(e.target.value)
+      }
+      this.calculator.sum = e.target.value
+    },
+    inputRate(e) {
+      e.target.value = e.target.value.replace(/[^\d.]/g, '')
+      e.target.value = e.target.value.replace(/\.{2,}/g, '.')
+      e.target.value = e.target.value
+        .replace('.', '$#$')
+        .replace(/\./g, '')
+        .replace('$#$', '.')
+      e.target.value = e.target.value.replace(/^(-)*(\d+)\.(\d\d).*$/, '$1$2.$3')
+      if (e.target.value.indexOf('.') < 0 && e.target.value !== '') {
+        e.target.value = parseFloat(e.target.value)
+      }
+      this.calculator.rate = e.target.value
+    },
+    inputDuration(e) {
+      e.target.value = e.target.value.replace(/[^\d.]/g, '')
+      e.target.value = e.target.value.replace(/\./g, '')
+      e.target.value = e.target.value.replace(/^(-)*(\d+)\.(\d\d).*$/, '$1$2.$3')
+      this.calculator.duration = e.target.value
+    },
     submitCalc() {
+      var reg = /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?$)/
+      if (!reg.test(this.calculator.sum)) {
+        this.errMsg.errSum = '请输入正确的出借金额'
+        return
+      } else {
+        this.errMsg.errSum = ''
+      }
+
+      if (!reg.test(this.calculator.rate)) {
+        this.errMsg.errRate = '请输入正确的历史平均年化收益率'
+        return
+      } else {
+        this.errMsg.errRate = ''
+      }
+      if (!this.calculator.duration) {
+        this.errMsg.errDuration = '请输入正确的出借期限'
+        return
+      } else {
+        this.errMsg.errDuration = ''
+      }
+
       let params = {
         amount: this.calculator.sum,
         yearRate: this.calculator.rate,
@@ -184,6 +247,9 @@ export default {
         duration: '',
         type: 2
       }
+      this.$refs.refSum.value = ''
+      this.$refs.refRate.value = ''
+      this.$refs.refDuration.value = ''
       this.termType = 1
       this.expectedRevenue = 0
       this.totalSum = 0
@@ -247,11 +313,64 @@ export default {
             display: flex;
             .el-form-item {
               flex: 1;
+              margin-bottom: 0;
               .el-form-item__label {
                 font-size: 18px;
               }
               .el-form-item__content {
+                position: relative;
                 width: 250px;
+                display: inline-table;
+                input {
+                  display: table-cell;
+                  vertical-align: middle;
+                  box-sizing: border-box;
+                  width: 100%;
+                  height: 40px;
+                  line-height: 40px;
+                  padding: 0 15px;
+                  outline: none;
+                  border: 1px solid #dcdfe6;
+                  border-radius: 4px;
+                  border-top-right-radius: 0;
+                  border-bottom-right-radius: 0;
+                  background-color: #fff;
+                  &:focus {
+                    border-color: #f7be39;
+                  }
+                }
+                .el-select {
+                  input {
+                    border-top-right-radius: 4px;
+                    border-bottom-right-radius: 4px;
+                  }
+                }
+                ::-webkit-input-placeholder {
+                  color: #dcdfe6;
+                }
+                :-moz-placeholder {
+                  color: #dcdfe6;
+                }
+                ::-moz-placeholder {
+                  color: #dcdfe6;
+                }
+                :-ms-input-placeholder {
+                  color: #dcdfe6;
+                }
+                .unit {
+                  display: table-cell;
+                  vertical-align: middle;
+                  height: 38px;
+                  line-height: 38px;
+                  padding: 0 8px;
+                  color: #cdcdcd;
+                  background-color: #f8f8f8;
+                  border: 1px solid #dcdfe6;
+                  border-left: 0;
+                  border-radius: 4px;
+                  border-top-left-radius: 0;
+                  border-bottom-left-radius: 0;
+                }
                 .el-input-group__append {
                   padding: 0 8px;
                   background-color: #f8f8f8;
@@ -280,6 +399,36 @@ export default {
               }
               &:nth-of-type(2) {
                 text-align: right;
+              }
+            }
+          }
+          &.err-wrapper {
+            overflow: hidden;
+            font-size: 0;
+            height: 50px;
+            line-height: 50px;
+            span {
+              display: inline-block;
+              vertical-align: top;
+              width: 50%;
+              color: red;
+              font-size: 12px;
+            }
+          }
+          &:nth-of-type(2) {
+            span {
+              &:nth-of-type(1) {
+                text-indent: 85px;
+              }
+              &:nth-of-type(2) {
+                text-indent: 190px;
+              }
+            }
+          }
+          &:nth-of-type(4) {
+            span {
+              &:nth-of-type(1) {
+                text-indent: 85px;
               }
             }
           }
