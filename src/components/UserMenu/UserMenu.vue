f2830e76d7b7356f74157dbd99f0d69272a6a91a
wangzhe <wangzhe02@dpandora.cn>
2019-02-18 10:56:32 +0800

    去掉账户等级下面我的推荐码

--- src/components/UserMenu/UserMenu.vue
+++ src/components/UserMenu/UserMenu.vue
@@ -10,9 +10,9 @@
       <div class="safe">
         <span>安全等级</span> <span class="level">{{ safetyLevel }}</span>
       </div>
-      <div class="my-referral-code" v-if="isShowReferralCode && referralCode">
+      <!-- <div class="my-referral-code" v-if="isShowReferralCode && referralCode">
         <span>我的推荐码</span> <span class="referral-code">{{ referralCode }} </span><span><i class="iconfont icon-more"></i></span>
-      </div>
+      </div> -->
     </header>
     <ul class="block">
       <li class="title"><i class="iconfont icon-account"></i> <span>我的账户</span></li>
