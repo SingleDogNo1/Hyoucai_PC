79f40942719ecd84f9fae7dfd308f3e580f1c480
hulixia@dpandora.cn <Jingyong666>
2019-03-01 10:47:11 +0800

    Merge branch 'dev_invest' of ssh://gitlab.dpandora.cn:10022/Front_End_Home/Hyoucai_PC into dev_invest
    # Conflicts:
    #	src/views/hyc/lend/easyDetail.vue

--- src/views/djs/lend/easyDetail.vue
+++ src/views/djs/lend/easyDetail.vue
@@ -83,11 +83,11 @@
               @click="handleInvest"
             >{{investBtn}}</button>
           </div>
+          <p class="err-msg" v-if="errMsg">{{errMsg}}</p>
           <p class="expected-profits">
             <span class="title">预期收益：</span>
             <span class="value">{{expectedIncome}}元</span>
           </p>
-          <p class="err-msg" v-if="errMsg">{{errMsg}}</p>
         </div>
       </div>
     </section>
@@ -1286,6 +1286,7 @@ export default {
           }
         }
         .err-msg {
+          margin-top: 13px;
           width: 100%;
           font-size: $font-size-small-ss;
           color: #e9122c;
