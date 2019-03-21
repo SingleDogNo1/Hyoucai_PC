c8c57f648f30f8f33a0408c7fdb86c02b9f1d9e6
zhaochenmin <zhaochenmin@dpandora.cn>
2019-03-21 15:23:41 +0800

    修改电子账户手机号

--- src/views/common/basicInfo/basicInfo.vue
+++ src/views/common/basicInfo/basicInfo.vue
@@ -58,11 +58,11 @@
           <span class="wrap_left">交易密码</span> <span class="wrap_center">{{ escrowAccountInfo.transPassword }}</span>
           <button class="wrap_btn" @click="tansactionPwd">修改</button>
         </div>
-        <!--<div class="wrap_rows last_rows">-->
-          <!--<span class="wrap_left">电子账户手机号</span>-->
-          <!--<span class="wrap_center">{{ escrowAccountInfo.mobile }}</span>-->
-          <!--<button class="wrap_btn" @click="changeDzPhone">修改</button>-->
-        <!--</div>-->
+        <div class="wrap_rows last_rows">
+          <span class="wrap_left">电子账户手机号</span>
+          <span class="wrap_center">{{ escrowAccountInfo.mobile }}</span>
+          <button class="wrap_btn" @click="changeDzPhone">修改</button>
+        </div>
       </div>
       <div class="openAccount" v-show="!flag">
         <div class="tips">
