51d0c7c7f85c73074ae5b9240fd65c6c7727e54e
zhaochenmin <zhaochenmin@dpandora.cn>
2019-05-27 10:41:25 +0800

    压缩图片的插件修改1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-// function kbs(n) {
-//   return 1024 * n
-// }
+function kbs(n) {
+  return 1024 * n
+}
 
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '../' : '/',
@@ -33,28 +33,28 @@ module.exports = {
       config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
     }
   },
-  // chainWebpack: config => {
-  //   if (process.env.VUE_APP_RUNTIME_ENV === 'production') {
-  //     const imagesRule = config.module.rule('images')
-  //     imagesRule.uses.clear()
-  //     imagesRule
-  //       .use('url-loader')
-  //       .loader('url-loader')
-  //       .options({
-  //         limit: kbs(4),
-  //         fallback: {
-  //           loader: 'file-loader',
-  //           options: {
-  //             name: 'img/[name].[hash:8].[ext]'
-  //           }
-  //         }
-  //       })
-  //     imagesRule
-  //       .use('image-webpack-loader')
-  //       .loader('image-webpack-loader')
-  //       .options({ bypassOnDebug: true })
-  //   }
-  // },
+  chainWebpack: config => {
+    if (process.env.VUE_APP_RUNTIME_ENV === 'production') {
+      const imagesRule = config.module.rule('images')
+      imagesRule.uses.clear()
+      imagesRule
+        .use('url-loader')
+        .loader('url-loader')
+        .options({
+          limit: kbs(4),
+          fallback: {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        })
+      imagesRule
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({ disable: true })
+    }
+  },
   devServer: {
     host: '0.0.0.0',
     disableHostCheck: true,
