e5bf7d1cd50017beb653ed509b7daac85bf2adf4
zhaochenmin <zhaochenmin@dpandora.cn>
2019-07-23 09:26:44 +0800

    修改了旧的公网备案号

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -67,7 +67,7 @@
         <a target="_blank" href="https://credit.szfw.org/CX20180307033115391632.html"></a>
         <a target="_blank" href="https://v.pinpaibao.com.cn/cert/site/?site=www.hyoucai.com&at=realname"></a>
         <a target="_blank" href="https://ss.knet.cn/verifyseal.dll?sn=e17033136010067212faqu000000&ct=df&a=1&pa=0.7946975489546229"></a>
-        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36010802000243"></a>
+        <a target="_blank" href="http://www.beian.gov.cn/portal/recordQuery?token=f8204fe9-01fa-49f6-a40f-21f49add7bd6"></a>
         <a target="_blank" href="http://www.jxcn.cn/jbzx/jbzn.htm"></a>
         <a target="_blank" href="https://www.hyoucai.com/djs/#/announcement/2346?paramCode=WZGG"></a>
         <a target="_blank" href="http://shuidi.cn/companyextreme-cba6d53b25e3568746166d73661021a7.html"></a>
